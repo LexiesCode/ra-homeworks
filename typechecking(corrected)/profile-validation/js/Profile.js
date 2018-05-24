5e47dbeebbdbeb5bc784f551d20c1e2871adec2d
LexiesCode <35810389+LexiesCode@users.noreply.github.com>
2018-05-24 23:09:08 +0300

    2.4.3 done

--- typechecking(corrected)/profile-validation/js/Profile.js
+++ typechecking(corrected)/profile-validation/js/Profile.js
@@ -27,3 +27,58 @@ const Profile = props => {
     </div>
   );
 };
+
+const bthDayPropType = (props, propName, componentName) => {
+  const bthDay = props[propName];
+  const isBthDay = (typeof bthDay === 'string') && /\d{4}\-\d{2}\-\d{2}/.test(bthDay);
+  const checkBthDay = new Date(bthDay) > new Date;
+    
+  if (!isBthDay) {
+    return new Error(`Неверный формат параметра ${propName} в компоненте ${componentName}: необходимо указать дату рождения в формате YYYY-MM-DD`);
+  } else if (checkBthDay) {
+    return new Error(`Неверно указан параметр ${propName} в компоненте ${componentName}: некорректно указана дата рождения`);
+  }
+  return null;
+}
+
+const createChainableTypeChecker = (validate) => {
+  const checkType = (isRequired, props, propName, componentName, ...rest) => {
+    if (!props[propName]) {
+      if (isRequired) {
+        return new Error(`Обязательный атрибут ${propName} не был передан компоненту ${componentName}`)
+      }
+      return null;
+    } else {
+      return validate(props, propName, componentName);
+    }
+  };
+  const chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+  return chainedCheckType;
+};
+
+const bthDayPropTypeChecker = createChainableTypeChecker(bthDayPropType);
+
+
+const urlPropType = (props, propName, componentName) => {
+  const url = props[propName];
+  const isUrl = (typeof url === 'string') && /https:\/\/vk.com\/(id[0-9]+|[A-Za-z0-9_-]+)/.test(url);
+  if (!isUrl) {
+    return new Error(`Неверный формат параметра ${propName} в компоненте ${componentName}: необходимо указать корректный url`);
+  }
+  return null;
+};
+
+Profile.propTypes = {
+  first_name: PropTypes.string,
+  last_name: PropTypes.string,
+  birthday: bthDayPropTypeChecker.isRequired,
+  img: PropTypes.string,
+  url: urlPropType
+}
+
+Profile.defaultProps = {
+  img: './images/profile.jpg',
+  birthday: '1988-03-21'
+}
+
