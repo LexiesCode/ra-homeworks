37007e0ccc8901738dc11be9129587b075bb6e6a
LexiesCode <35810389+LexiesCode@users.noreply.github.com>
2018-06-04 16:56:45 +0300

    some corrections

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,18 +1,15 @@
 'use strict';
 const AuthForm = ({onAuth}) => {
-    let nameField,
-    emailField,
-    passwordField;
-
-    function authorizeUser(e) {
+    
+    const authorizeUser = (e) => {
         e.preventDefault();
         if (onAuth && typeof onAuth === 'function') {
+            const formFields = e.currentTarget.elements;
             const user = {
-                name: nameField.value,
-                email: emailField.value,
-                password: passwordField.value
-            };
-        
+                name: formFields.name.value,
+                email: formFields.email.value,
+                password: formFields.password.value
+            }
         onAuth(user);
         }
     }
@@ -25,32 +22,34 @@ const AuthForm = ({onAuth}) => {
         e.currentTarget.value = e.currentTarget.value.replace(/\W/gi, '');
     }
 
-    const name = (
-        <div className="Input">
-                <input ref={element => nameField = element} required type="text" placeholder="Имя"/>
-                <label></label>
-        </div>
-    );
-
-    const email = (
-        <div className="Input">
-                <input ref={element => emailField = element} type="email" onChange={validateEmail} placeholder="Электронная почта"/>
-                <label></label>
-        </div>
-    );
-
-    const password = (
-        <div className="Input">
-                <input ref={element => passwordField = element} required type="password" onChange={validatePassword} placeholder="Пароль"/>
-                <label></label>
-        </div>
-    );
-
+   
     return (
         <form className="ModalForm" onSubmit={authorizeUser} action="/404/auth/" method="POST">
-            {name}
-            {email}
-            {password}
+            <div className="Input">
+                <input 
+                    required 
+                    type="text" 
+                    placeholder="Имя"
+                />
+                <label></label>
+            </div>
+            <div className="Input">
+                <input 
+                    type="email" 
+                    onChange={validateEmail} 
+                    placeholder="Электронная почта"
+                />
+                <label></label>
+        </div>
+        <div className="Input">
+                <input 
+                    required 
+                    type="password" 
+                    onChange={validatePassword} 
+                    placeholder="Пароль"
+                />
+                <label></label>
+        </div>
             <button type="submit">
                 <span>Войти</span>
                 <i className="fa fa-fw fa-chevron-right"></i>
